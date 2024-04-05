2d66f56d512f5df01b7dbd9f0f3291e489ab9b3c
Mathieu Lecarme <mathieu@garambrogne.net>
2024-04-06 00:39:04 +0200

    fix: just loop over the lyaers in the manifest

--- tar2img.sh
+++ tar2img.sh
@@ -2,11 +2,23 @@
 
 set -e
 
-mkdir -p /data/root
-cd /data/root && tar -xvf /out/the_layer.tar
-cd /out
-make_ext4fs -l 1G -b 64k -L stream -g 256 root.img
+# / and : are boring in file name, lets replace them with _
+flat_name=$(echo "$1" | sed 's/[\/:]/_/g')
+manifest="manifests/$flat_name"
+layers=$( jq -r '.layers.[].digest' < "$manifest" )
+image=out/${flat_name}.img
+cwd=$(pwd)
+
+make_ext4fs -l 1G -b 64k -L stream -g 256 "$image"
+
 mkdir -p /tmp/disk
-fuse2fs  root.img /tmp/disk -o rw
-cp -av /data/root/* /tmp/disk/
+fuse2fs  "$image" /tmp/disk -o rw
+cd /tmp/disk
+
+for layer in $layers; do
+    # FIXME .wh. name must be handled
+    tar -xvzf "$cwd/layers/$layer"
+done
+
+cd "$cwd"
 umount /tmp/disk
