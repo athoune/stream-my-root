7600727bb294c73a37a91f7993c2277f40bfacad
Mathieu Lecarme <mathieu@garambrogne.net>
2024-04-05 23:15:15 +0200

    Code comments. Hide target folders.

--- manifest2layers.sh
+++ manifest2layers.sh
@@ -1,15 +1,27 @@
 #! /bin/bash
-
+#
+# Depends on :
+# - crane from https://github.com/google/go-containerregistry
+# - jq
+#
+# Usage example :
+# $ ./manifest2layers.sh debian:12-slim
+#
+# The script fetch the manifest for linux/amd64, et and lazy get the layers.
+# Datas are stored in layers and manifests folders.
+#
 set -e
 
 mkdir -p layers
 mkdir -p manifests
 
+# / and : are boring in file name, lets replace them with _
 flat_name=$(echo "$1" | sed 's/[\/:]/_/g')
+manifest="manifests/$flat_name"
 
-crane manifest --platform linux/amd64 "$1" > "manifests/$flat_name"
+crane manifest --platform linux/amd64 "$1" > $manifest
 
-layers=$( jq -r '.layers.[].digest' < "manifests/$flat_name" )
+layers=$( jq -r '.layers.[].digest' < $manifest )
 for layer in $layers; do
     if [ ! -e "layers/$layer" ]; then
         echo "Fetch layer ${layer}"
