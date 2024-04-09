cd9df3799a26eb923094c5a25eeac130c0e38f71
Mathieu Lecarme <mathieu@garambrogne.net>
2024-04-09 11:12:55 +0200

    feat: handle *

--- cmd/chunk/chunk.go
+++ cmd/chunk/chunk.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/athoune/stream-my-root/pkg/blocks"
 )
 
 func main() {
-	err := blocks.ChunkRawFile(os.Args[1])
-	if err != nil {
-		panic(err)
+	for _, img := range os.Args[1:] {
+		_, err := os.Stat(fmt.Sprintf("%s.recipe", img))
+		if err != nil && os.IsNotExist(err) { // FIXME handle other errors
+			err = blocks.ChunkRawFile(img)
+			if err != nil {
+				fmt.Printf("🐞 %s : %s\n", img, err)
+				continue
+			}
+			fmt.Println("✅ ", img)
+		} else {
+			fmt.Println("☁️ ", img)
+		}
 	}
 }
