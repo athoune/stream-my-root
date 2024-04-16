c0e31874bc196926cac21d6e96034c4ca53ebcd7
Mathieu Lecarme <mathieu@garambrogne.net>
2024-04-16 23:47:03 +0200

    fix: Chunker has test.

--- pkg/chunk/chunk_test.go
+++ pkg/chunk/chunk_test.go
@@ -1,6 +1,8 @@
 package chunk
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,4 +11,22 @@ import (
 func TestTrim(t *testing.T) {
 	assert.Equal(t, Rtrim([]byte{0, 0, 1, 0, 2, 0, 0}), []byte{0, 0, 1, 0, 2})
 	assert.Empty(t, Rtrim([]byte{0, 0, 0, 0}))
+}
+
+func TestChunker(t *testing.T) {
+	f, err := os.MkdirTemp("/tmp", "smr")
+	assert.NoError(t, err)
+	defer os.Remove(f)
+
+	r := make([]byte, 16)
+	for i := 0; i < len(r); i++ {
+		r[i] = byte(i)
+	}
+	raw := fmt.Sprintf("%s/raw", f)
+	err = os.WriteFile(raw, r, 0660)
+	assert.NoError(t, err)
+	c := NewChunker(8)
+	c.folder = f
+	err = c.ChunkRawFile(raw)
+	assert.NoError(t, err)
 }
